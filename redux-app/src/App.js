3f9822524ad9801e059c458eb5e68cfafc59d4a9
Tzuzul Code <mail@tzuzulcode.com>
2022-02-23 20:00:29 -0600

    Dispatch y selector con Redux

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,8 +1,24 @@
+import {useDispatch, useSelector} from 'react-redux'
+import { login, logout } from './features/user/userSlice';
+
 function App() {
+  
+  // ¿Quiero consultar el estado global?
+  const user = useSelector((state)=>state.user)
+
+  // ¿Quiero actualizar el estado global?
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-     <p>Hola</p>
+      {console.log(user)}
+     <p>Usuario: {user.logged?"Bienvenido "+user.name:"Inicia sesión"}</p>
+     <button onClick={()=>{dispatch(login("Tzuzul"))}}>Iniciar sesión</button>
+     <button onClick={()=>{dispatch(logout())}}>Cerrar sesión</button>
     </div>
+
+    // Agregar un router: Pagina principal, Pagina de inicio de sesión. 
+    // Iconos de redes sociales: Google, Facebook, Twitter, GitHub
   );
 }
 
--- redux-app/src/features/user/userSlice.js
+++ redux-app/src/features/user/userSlice.js
@@ -3,17 +3,20 @@ import {createSlice} from '@reduxjs/toolkit'
 const userSlice = createSlice({
     name:"user",
     initialState:{
-        logged:false
+        logged:false,
+        name:""
     },
     reducers:{
         login(state,action){
             state.logged = true
+            state.name = action.payload
         },
         logout(state,action){
             state.logged = false
+            state.name = ""
         }
     }
 })
 
-export const {login,logout} = userSlice.actions
-export default userSlice.reducer
+export const {login,logout} = userSlice.actions // Esto se utiliza en el dispatch
+export default userSlice.reducer // Esto en el store
