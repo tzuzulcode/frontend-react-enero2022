16a39ec550cf5ed08b67fa15c5cbf0be31368a21
Tzuzul Code <mail@tzuzulcode.com>
2022-04-08 20:19:45 -0500

    Testing

--- testing/src/calc.test.js
+++ testing/src/calc.test.js
@@ -1,5 +1,33 @@
 import Calculator from "./calc"
 
+const mock = jest.fn() // Crear una funcion
+const fetch = jest.fn() // Crear una funcion
+
+const fakeAdd = mock.mockImplementation((a,b)=>{
+    return a+b
+}) // Mock function
+
+const fakeRequest = fetch.mockImplementation(
+    ()=>Promise.resolve({name:"Tzuzul"})
+) // Stub
+
+//Matcher custom
+expect.extend({
+    toMatchJSON(received,argument){
+        if(received===argument){
+            return {
+                pass:true,
+                message:()=>'Matched'
+            }
+        }else{
+            return {
+                pass:false,
+                message:()=>'No matched'
+            }
+        }
+    }
+})
+
 describe("calculator",()=>{
     let add,sub
     beforeEach(()=>{
@@ -22,6 +50,53 @@ describe("calculator",()=>{
     it("should perform subtraction",()=>{
         expect(sub(5,5)).toEqual(0)
     })
+
+    it("object in array",()=>{
+        const users = [
+            {
+                name:"Tzuzul"
+            }
+        ]
+
+        let a = {
+            name:"Tzuzul"
+        }
+
+        let b = {
+            name:"Tzuzul"
+        }
+
+        expect(a.name).toBeDefined() // Verifica si cierta valor no es nula
+
+
+        if (a==b){
+
+        } //false
+
+        let arreglo = [a,b]
+
+        arreglo[0]==={
+            name:"Tzuzul"
+        }//falso
+
+        expect(users).toContainEqual({
+            name:"Tzuzul"
+        }) // Extra: Compara el contenido de los objetos/arreglos
+
+        // expect(users).toContain(users[0])
+    })
+
+    it("fake add should add two values",()=>{
+        expect(fakeAdd(5,5)).toEqual(10)
+    })
+
+    it("Fake request should resolve the user",()=>{
+        fakeRequest()
+        .then(x=>console.log(x))
+        expect(fakeRequest).toHaveBeenCalled()
+    })
+
+
 })
 
 // describe('calculator', () => { 
